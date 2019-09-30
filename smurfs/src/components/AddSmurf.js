b48c53353a4f13708169de93711d94736fc585c2
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-29 21:37:43 -0400

    adds new component AddSmurf, which will hold the input field for entering information about a new smurf

---
+++ smurfs/src/components/AddSmurf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useState} from 'react';
+
+const AddSmurf = () => {
+    const [newSmurf, setNewSmurf] = useState("");
+    return (
+        <input type="text"/>
+
+    )
+}
