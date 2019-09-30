9565b366960ade1cc0b217c9ac2d6cdd94a43179
triggering-event <gabriel.j.shapiro@gmail.com>
2019-09-29 21:22:06 -0400

    adds initial state consisting of empty smurfs array to reducer

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,9 +1,11 @@
 import {FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAIL} from "../actions/getSmurfs";
 import {POST_SMURFS_START, POST_SMURFS_SUCCESS, POST_SMURFS_FAIL} from "../actions/postSmurfs";
 
-;
+const initialState = {
+    smurfs: []
+}
 
-const reducer = (state /*=initialState*/, action) => {
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_SMURFS_START:
             return {
